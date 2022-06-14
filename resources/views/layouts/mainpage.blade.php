73ea71c11464aa7c91abd89511a51debde5cdeb4
Hanis Batrisyia <hanisbtrsyia@gmail.com>
2022-06-15 00:09:59 +0800

    update cart

--- resources/views/layouts/mainpage.blade.php
+++ resources/views/layouts/mainpage.blade.php
@@ -78,7 +78,7 @@
                     <div class="col-lg-4 col-sm-6 col-12">
                         <div class="widgets-wrap float-md-right">
                             <div class="widget-header  mr-3">
-                                <a href="#" class="icon icon-sm rounded-circle border"><i
+                                <a href="{{ route('cart') }}" class="icon icon-sm rounded-circle border"><i
                                         class="fa fa-shopping-cart"></i></a>
                                 <span class="badge badge-pill badge-danger notify">0</span>
                             </div>
